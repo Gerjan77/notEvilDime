1976bee3cb01f7651d308d2e155f311425e8cb63
GeertJan <geertjan@Targa-Linux.(none)>
2017-12-09 02:06:28 +0100

    Build

--- build/ui_sendcoinsentry.h
+++ build/ui_sendcoinsentry.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'sendcoinsentry.ui'
 **
-** Created: Sun Nov 26 05:05:45 2017
+** Created: Sat Dec 9 01:35:36 2017
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
